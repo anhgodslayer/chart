f7449864eae9e67bc85324a769bc35ca991e3aa1
anhgodslayer <111446011+anhgodslayer@users.noreply.github.com>
2023-03-16 16:08:14 +0700

    Add files via upload

--- chartapp/models.py
+++ chartapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class show(models.Model):
+ title=models.CharField(max_length=20)
+ class Meta:  
+        db_table = "web"
--- chartapp/views.py
+++ chartapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
+from .models import show
+from django.db import connection
 # Create your views here.
 def index(request):
     data = "Current Data"
-
+    zoneH=show.objects.all().count()
     context= {
-        "data": data
+        "data": data,
+        "zoneH": zoneH,
     }
+   
     return render(request, 'chartapp/index.html', context)
